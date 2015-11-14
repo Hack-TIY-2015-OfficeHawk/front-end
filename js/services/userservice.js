5174a1926e752c077c67c5940852f5ba9257fdc0
Cori Fowler <fowler.cori@gmail.com>
2015-11-14 09:52:32 -0500

    updated user service with login/logout functions

--- js/services/userservice.js
+++ js/services/userservice.js
@@ -1,4 +1,42 @@
 let UserService = function($http, SERVER, $cookies, $state) {
+
+  console.log(SERVER);
+
+  // check for auth token
+
+  this.checkAuth = function () {
+    let token = $cookies.get('auth-Token');
+
+    token = SERVER.CONFIG.headers['X-AUTH-TOKEN'];
+
+    if (token) {
+      return $http.get(SERVER.URL + 'check' + SERVER.CONFIG);
+    } else {
+      $state.go('root.home');
+    }
+  };
+
+  // user login
+
+  this.userLogin = function (userObj) {
+    return $http.post(SERVER.URL + 'login', userObj, SERVER.CONFIG);
+  };
+
+  this.userSuccess = function (res) {
+    $cookies.put('auth-Token', res.data.auth_token);
+    SERVER.CONFIG.headers['X-AUTH-TOKEN'] = res.data.auth_token;
+    $state.go('root.admin');
+  };
+
+  // user logout
+
+  this.userLogout = function () {
+    $cookies.remove('auth-Token');
+    SERVER.CONFIG.headers['X-AUTH-TOKEN'] = null;
+    $state.go('root.home');
+  };
+
+  // register a new user/manager
   
   let User = function (obj) {
     this.userName = obj.username;
