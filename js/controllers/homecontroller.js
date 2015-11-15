13d608826ff41ecda20f2a85a5b8720fc4ae9a91
Vinnie Dolan <vpdolan@gmail.com>
2015-11-15 09:21:25 -0500

    tweaking home files

--- js/controllers/homecontroller.js
+++ js/controllers/homecontroller.js
@@ -1,9 +1,38 @@
+import $ from 'jquery';
+
 let HomeController = function($scope) {
-  
-  
+  var mouseoversound=createsoundbite('images/Screaming Hawk 2-SoundBible.com-1003225740.mp3');
+  var clicksound=createsoundbite('click.mp3'); 
 
 };
 
+  
+
+  // var audio = $("images/Screaming Hawk 2-SoundBible.com-1003225740.mp3")[0];
+  //   $(".button").mouseenter(function() {
+  //   audio.play();
+  //  }); 
+  
 HomeController.$inject = ['$scope'];
 
 export default HomeController;
+
+
+
+
+
+
+// function createsoundbite(sound){
+//   var html5audio=document.createElement('audio');
+//     }
+//     html5audio.load()
+//     html5audio.playclip=function() {
+//       html5audio.currentTime=0
+//       html5audio.play()
+//     };
+//     return html5audio
+//   };
+  
+
+
+// };
